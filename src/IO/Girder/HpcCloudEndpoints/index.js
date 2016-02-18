0c52355ce01eab12f2016b965a26d48ef34fa8d4
TristanWright <tristan.wright@kitware.com>
2016-02-17 17:39:14 -0700

    fix(hpccloud): added taskflow endpoints

--- src/IO/Girder/HpcCloudEndpoints/index.js
+++ src/IO/Girder/HpcCloudEndpoints/index.js
@@ -2,10 +2,12 @@ import aws          from './aws';
 import clusters     from './clusters';
 import projects     from './projects';
 import simulations  from './simulations';
+import taskflows    from './taskflows';
 
 export default [
     aws,
     clusters,
     projects,
     simulations,
+    taskflows,
 ];
---
+++ src/IO/Girder/HpcCloudEndpoints/taskflows.js
@@ -0,0 +1,52 @@
+export default function({client, filterQuery, mustContain, busy}) {
+    return {
+
+        //POST /taskflows Create the taskflow
+        createTaskflow(taskFlowClass) {
+            return busy(client._.post('/taskflows', {taskFlowClass}));
+        },
+
+        //GET /taskflows/{id} Get a taskflow
+        getTaskflow(id, path) {
+            if (path) {
+                return busy(client._.get(`/taskflows/${id}?path=${path}`));
+            }
+            return busy(client._.get(`/taskflows/${id}`));
+        },
+
+        //PATCH /taskflows/{id} Update the taskflow
+        updateTaskflow(id, params) {
+            return busy(client._.patch(`/taskflows/${id}`, params));
+        },
+
+        //DELETE /taskflows/{id} Delete the taskflow
+        deleteTaskflow(id) {
+            return busy(client._.delete(`/taskflows/${id}`));
+        },
+
+        //PUT /taskflows/{id}/start Start the taskflow
+        startTaskflow(id) {
+            return busy(client._.put(`/taskflows/${id}/start`));
+        },
+
+        //GET /taskflows/{id}/status Get the taskflow status
+        getTaskflowStatus(id) {
+            return busy(client._.get(`/taskflows/${id}/status`));
+        },
+
+        //GET /taskflows/{id}/tasks Get all the tasks associated with this taskflow
+        getTaskflowTaskStatuses(id) {
+            return busy(client._.get(`/taskflows/${id}/task`));
+        },
+
+        //POST /taskflows/{id}/tasks Create a new task associated with this flow
+        createNewTaskForTaskflow(id, params) {
+            return busy(client._.post(`/taskflows/${id}/tasks`, params));
+        },
+
+        //PUT /taskflows/{id}/terminate Terminate the taskflow
+        endTaskflow(id) {
+            return busy(client._.put(`/taskflows/${id}/terminate`));
+        },
+    };
+}
