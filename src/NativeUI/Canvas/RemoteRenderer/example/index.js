9005d18634ef9b4ec2de70adb880dbe561b044b1
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-04-11 15:51:25 -0600

    feat(RemoteRenderer): Add new RemoteRenderer
    RemoteRenderer provide a similar implementation of the ParaViewWeb ImageDeliveryViewport.

---
+++ src/NativeUI/Canvas/RemoteRenderer/example/index.js
@@ -0,0 +1,32 @@
+import RemoteRenderer from '..';
+import { onSizeChange, startListening } from '../../../../Common/Misc/SizeHelper';
+import SmartConnect from '../../../../IO/WebSocket/SmartConnect';
+import ParaViewWebClient from '../../../../IO/WebSocket/ParaViewWebClient';
+
+document.body.style.padding = '0';
+document.body.style.margin = '0';
+
+const divRenderer = document.createElement('div');
+document.body.appendChild(divRenderer);
+
+divRenderer.style.position = 'relative';
+divRenderer.style.width = '100vw';
+divRenderer.style.height = '100vh';
+divRenderer.style.overflow = 'hidden';
+
+const config = { sessionURL: 'ws://localhost:1234/ws' };
+const smartConnect = new SmartConnect(config);
+smartConnect.onConnectionReady(connection => {
+  const pvwClient = ParaViewWebClient.createClient(connection, ['MouseHandler', 'ViewPort', 'ViewPortImageDelivery']);
+  const renderer = new RemoteRenderer(pvwClient);
+  renderer.setContainer(divRenderer);
+  renderer.onImageReady(() => {
+    console.log('We are good');
+  });
+  window.renderer = renderer;
+  onSizeChange(() => {
+    renderer.resize();
+  });
+  startListening();
+});
+smartConnect.connect();
