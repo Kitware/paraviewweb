0654ca828ae16316b5a2215ab48f7a344dc74a5c
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-08-03 10:00:45 -0600

    fix(ScoresProvider): Add ScoresProvider

---
+++ src/Common/Misc/AnnotationBuilder/index.js
@@ -0,0 +1,49 @@
+// ----------------------------------------------------------------------------
+// Internal helpers
+// ----------------------------------------------------------------------------
+
+let generation = 0;
+
+// ----------------------------------------------------------------------------
+// Public builder method
+// ----------------------------------------------------------------------------
+
+export function annotation(selection, score, weight = 1, rationale = '') {
+  generation++;
+  return {
+    generation,
+    selection,
+    score,
+    weight,
+    rationale,
+  };
+}
+
+// ----------------------------------------------------------------------------
+
+export function update(annotationObject, changeSet) {
+  const updatedAnnotation = Object.assign({}, annotation, changeSet);
+
+  let changeDetected = false;
+  Object.keys(updatedAnnotation).forEach(key => {
+    if (updatedAnnotation[key] !== annotationObject[key]) {
+      changeDetected = true;
+    }
+  });
+
+  if (changeDetected) {
+    generation++;
+    updatedAnnotation.generation = generation;
+  }
+
+  return updatedAnnotation;
+}
+
+// ----------------------------------------------------------------------------
+// Exposed object
+// ----------------------------------------------------------------------------
+
+export default {
+  annotation,
+  update,
+};
