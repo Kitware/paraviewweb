1b31c2f1c05354d04a1c03f5e2e0a7fc119732e4
Travis CI <sebastien.jourdain@kitware.com>
2016-09-30 21:23:33 +0000

    Updates

--- examples/HistogramSelector/HistogramSelector.js
+++ examples/HistogramSelector/HistogramSelector.js
@@ -43050,7 +43050,7 @@
 
 	    // don't replace the default region with an empty region, so UI can display the default region.
 	    if (regions.length > 0 && !(regions.length === 1 && regions[0] === model.defaultScore)) {
-	      def.regions = regions;
+	      def.regions = [].concat(regions);
 	      def.dividers = dividers;
 	    }
 	  }
