13868cdb37b5f54065438e644f148656f7272fb6
Aron Helser <aron.helser@kitware.com>
2016-11-14 17:02:18 -0500

    fix(TextInputWidget): allow width, icon to be set via props, Enter/Esc handled
    max-width on the input element,
    icon for ending the edit, useful for a search icon.
    
    Also listen for keystrokes and end editing when Enter
    is pressed, and revert to the inital value on Esc.

--- src/React/Widgets/TextInputWidget/index.js
+++ src/React/Widgets/TextInputWidget/index.js
@@ -10,12 +10,15 @@ export default React.createClass({
     name: React.PropTypes.string,
     onChange: React.PropTypes.func,
     value: React.PropTypes.string,
+    maxWidth: React.PropTypes.string,
+    icon: React.PropTypes.string,
   },
 
   getDefaultProps() {
     return {
       value: '',
       className: '',
+      icon: 'fa-check',
     };
   },
 
@@ -41,17 +44,32 @@ export default React.createClass({
     }
   },
 
+  handleKeyUp(e) {
+    if (!this.textInput) return;
+    if (e.key === 'Enter' || e.key === 'Return') {
+      this.textInput.blur();
+    } else if (e.key === 'Escape') {
+      this.setState({ valueRep: this.props.value });
+    }
+  },
+
   render() {
+    const inlineStyle = this.props.maxWidth ? { maxWidth: this.props.maxWidth } : {};
     return (
       <div className={[style.container, this.props.className].join(' ')}>
         <input
           className={style.entry}
           type="text"
           value={this.state.editing ? this.state.valueRep : this.props.value}
+          style={inlineStyle}
           onChange={this.valueChange}
           onBlur={this.endEditing}
+          onKeyUp={this.handleKeyUp}
+          ref={(c) => { this.textInput = c; }}
         />
-        <i className={this.state.editing ? style.editingButton : style.button} />
+        { // Use the check icon by default, but allow customization, for example: fa-search
+        }
+        <i className={[this.state.editing ? style.editingButton : style.button, this.props.icon].join(' ')} />
       </div>);
   },
 });
