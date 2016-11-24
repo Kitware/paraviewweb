4dc9ffe70e97fbc355bf204f955e6b09d99fdd14
David Thompson <david.thompson@kitware.com>
2016-11-24 12:24:06 -0500

    Progress on hyperbolic edge bundles.

---
+++ src/InfoViz/Native/HyperbolicEdgeBundles/example/index.js
@@ -0,0 +1,90 @@
+import 'normalize.css';
+
+import HyperbolicEdgeBundles from '..';
+import Workbench from '../../../../Component/Native/Workbench';
+import BackgroundColor from '../../../../Component/Native/BackgroundColor';
+import ToggleControl from '../../../../Component/Native/ToggleControl';
+import Spacer from '../../../../Component/Native/Spacer';
+import Composite from '../../../../Component/Native/Composite';
+import ReactAdapter from '../../../../Component/React/ReactAdapter';
+import WorkbenchController from '../../../../Component/React/WorkbenchController';
+
+import { debounce } from '../../../../Common/Misc/Debounce';
+
+const container = document.querySelector('.content');
+container.style.height = '100vh';
+container.style.width = '100vw';
+
+const green = BackgroundColor.newInstance({ color:'green' });
+const   red = BackgroundColor.newInstance({ color:'red' });
+const  blue = BackgroundColor.newInstance({ color:'blue' });
+const  pink = BackgroundColor.newInstance({ color:'pink' });
+const  gray = BackgroundColor.newInstance({ color:'gray' });
+
+const hyperbolicView = HyperbolicEdgeBundles.newInstance();
+
+const viewports = {
+  Gray: {
+    component: gray,
+    viewport: 2,
+  },
+  HyperbolicEdgeBundles: {
+    component: hyperbolicView,
+    viewport: 0,
+  },
+  Green: {
+    component: green,
+    viewport: -1,
+  },
+  Red: {
+    component: red,
+    viewport: -1,
+  },
+  Blue: {
+    component: blue,
+    viewport: 1,
+  },
+  Pink: {
+    component: pink,
+    viewport: 3,
+  },
+};
+
+const workbench = new Workbench();
+workbench.setComponents(viewports);
+workbench.setLayout('2x2');
+
+const props = {
+  onLayoutChange(layout) {
+    workbench.setLayout(layout);
+  },
+  onViewportChange(index, instance) {
+    workbench.setViewport(index, instance);
+  },
+  activeLayout: workbench.getLayout(),
+  viewports: workbench.getViewportMapping(),
+  count: 4,
+};
+
+const controlPanel = new ReactAdapter(WorkbenchController, props);
+const shiftedWorkbench = new Composite();
+shiftedWorkbench.addViewport(new Spacer(), false);
+shiftedWorkbench.addViewport(workbench);
+const mainComponent = new ToggleControl(shiftedWorkbench, controlPanel, 280);
+mainComponent.setContainer(container);
+
+workbench.onChange(model => {
+  props.activeLayout = model.layout;
+  props.viewports = model.viewports;
+  props.count = model.count;
+  controlPanel.render();
+});
+
+// Create a debounced window resize handler
+const resizeHandler = debounce(() => {
+  mainComponent.resize();
+}, 50);
+
+// Register window resize handler so workbench redraws when browser is resized
+window.onresize = resizeHandler;
+
---
+++ src/InfoViz/Native/HyperbolicEdgeBundles/index.js
@@ -0,0 +1,176 @@
+import d3 from 'd3';
+
+import style from 'PVWStyle/InfoVizNative/HyperbolicEdgeBundles.mcss';
+import htmlContent from './body.html';
+import CompositeClosureHelper from '../../../Common/Core/CompositeClosureHelper';
+import {
+  // vectorMag,
+  vectorDiff,
+  vectorMAdd,
+  // vectorDot,
+  vectorScale,
+  // pointsApproxCollinear,
+  // lineLineIntersectFragile,
+  // lineCircleIntersectFragile,
+  hyperbolicPlanePointsToPoincareDisk,
+  // hyperbolicPlaneGeodesicOnPoincareDisk,
+  // interpolateOnPoincareDisk,
+} from '../../../Common/Misc/HyperbolicGeometry';
+
+
+function hyperbolicEdgeBundle(publicAPI, model) {
+  publicAPI.resize = () => {
+    if (!model.container) {
+      return;
+    }
+
+    const rect = model.container.getBoundingClientRect();
+    d3.select(model.container).select('svg')
+      .attr('width', rect.width)
+      .attr('height', rect.height);
+    model.transformGroup.attr('transform',
+      `scale(${rect.width / 2.0}, ${rect.height / 2.0}) translate(1, 1)`);
+  };
+
+  publicAPI.setContainer = (el) => {
+    if (model.container) {
+      while (model.container.firstChild) {
+        model.container.removeChild(model.container.firstChild);
+        delete model.unselectedBundleGroup;
+        delete model.treeEdgeGroup;
+        delete model.selectedBundleGroup;
+        delete model.nodeGroup;
+        delete model.transformGroup;
+        // TODO: When el is null/undefined and API is provider, unsubscribe.
+      }
+    }
+
+    model.container = el;
+
+    if (model.container) {
+      // Create placeholder
+      model.container.innerHTML = htmlContent;
+
+      // Apply style and create SVG for rendering
+      const viewdiv = d3.select(model.container).select('.hyperbolic-geom-viewport');
+      viewdiv.classed(style.hyperbolicGeomContainer, true);
+      const svg = viewdiv.append('svg').classed('hyperbolic-geom-svg', true);
+      model.transformGroup = svg.append('g').classed('disk-transform', true);
+      model.unselectedBundleGroup = model.transformGroup.append('g').classed('unselected-bundles', true);
+      model.treeEdgeGroup = model.transformGroup.append('g').classed('tree-edges', true);
+      model.selectedBundleGroup = model.transformGroup.append('g').classed('selected-bundles', true);
+      model.nodeGroup = model.transformGroup.append('g').classed('nodes', true);
+
+      publicAPI.resize(); // Apply a transform to the transformGroup based on the size of the SVG.
+
+      // Instead of getting data from a provider:
+      //   if (model.provider.isA('MutualInformationSummaryProvider')) ...
+      // Hardcode it into the model for now:
+      model.nodes = [
+        [326.68762207031250, 350.63452148437500],
+        [245.65452575683594, 160.70016479492188],
+        [373.11471557617190, 257.08932495117190],
+        [409.98583984375000, 253.11964416503906],
+        [319.04360961914060, 315.44680786132810],
+        [356.74649047851560, 218.36364746093750],
+        [413.31097412109375, 354.94842529296875],
+        [305.22348022460940, 380.38299560546875],
+        [309.88809204101560, 187.03582763671875],
+        [301.91839599609375, 218.60316467285156],
+        [334.46783447265625, 257.22268676757810],
+        [382.04049682617190, 185.76583862304688],
+        [346.40429687500000, 311.91351318359375],
+        [298.08831787109375, 271.03237915039060],
+        [377.40567016601560, 347.31036376953125],
+        [373.38021850585940, 304.53674316406250],
+        [384.66232299804690, 383.36221313476560],
+        [271.03741455078125, 186.00474548339844],
+        [269.68402099609375, 224.66835021972656],
+        [263.04452514648440, 285.27932739257810],
+        [404.71112060546875, 158.82929992675780],
+      ];
+      model.treeEdges = [
+        [334.18762488572924, 358.13452292263090, 312.72348495611050, 387.88299104668560],
+        [334.18762488572924, 358.13452292263090, 353.90429994195296, 319.41352650362420],
+        [253.15452023990338, 168.20016610150236, 278.53741891587500, 193.50474848244320],
+        [380.61471694769847, 264.58933353902040, 417.48583350629195, 260.61963708034210],
+        [380.61471694769847, 264.58933353902040, 341.96783106088634, 264.72268514753340],
+        [326.54360452297306, 322.94680128722155, 353.90429994195296, 319.41352650362420],
+        [364.24649860818110, 225.86363986467606, 341.96783106088634, 264.72268514753340],
+        [364.24649860818110, 225.86363986467606, 389.54050702058120, 193.26583277340492],
+        [420.81098775460663, 362.44843078875480, 384.90567631119114, 354.81036757015187],
+        [317.38810462926050, 194.53583132489254, 309.41841065363536, 226.10316106037124],
+        [309.41841065363536, 226.10316106037124, 341.96783106088634, 264.72268514753340],
+        [309.41841065363536, 226.10316106037124, 278.53741891587500, 193.50474848244320],
+        [309.41841065363536, 226.10316106037124, 277.18402873004840, 232.16835229042620],
+        [341.96783106088634, 264.72268514753340, 353.90429994195296, 319.41352650362420],
+        [341.96783106088634, 264.72268514753340, 305.58830280627257, 278.53238498678960],
+        [389.54050702058120, 193.26583277340492, 412.21111372773570, 166.32929457453255],
+        [353.90429994195296, 319.41352650362420, 384.90567631119114, 354.81036757015187],
+        [353.90429994195296, 319.41352650362420, 380.88020608315810, 312.03672828584430],
+        [305.58830280627257, 278.53238498678960, 270.54451200408226, 292.77934091175390],
+        [384.90567631119114, 354.81036757015187, 392.16231669382640, 390.86222345739990],
+      ];
+      model.hyperbolicBounds = [[253.15, 166.33], [420.81, 390.86]];
+      model.focus = vectorScale(0.5, vectorMAdd(model.hyperbolicBounds[0], model.hyperbolicBounds[1], 1.0));
+      model.diskScale = vectorDiff(model.hyperbolicBounds[0], model.hyperbolicBounds[1]).reduce((a, b) => ((a > b) ? a : b)) / 2.0;
+      publicAPI.modelUpdated();
+    }
+  };
+
+  publicAPI.coordsChanged = () => {
+    model.nodeGroup.selectAll('.node').data(model.diskCoords, i => i);
+      model.nodeGroup.selectAll('.node') //.transition().duration(100)
+        .attr('cx', d => d[0])
+        .attr('cy', d => d[1]);
+  };
+
+  publicAPI.focusChanged = () => {
+    model.diskCoords = hyperbolicPlanePointsToPoincareDisk(
+      model.nodes, model.focus, model.diskScale);
+    publicAPI.coordsChanged();
+  };
+
+  publicAPI.modelUpdated = () => {
+    model.diskCoords = hyperbolicPlanePointsToPoincareDisk(
+      model.nodes, model.focus, model.diskScale);
+    const ngdata = model.nodeGroup.selectAll('.node').data(model.diskCoords);
+    ngdata.enter().append('circle')
+      .classed('node', true)
+      .classed(style.hyperbolicNode, true)
+      .attr('r', '0.03px')
+      .on('click', (d, i) => { model.focus = model.nodes[i]; console.log('focus now ', model.focus); publicAPI.focusChanged(); });
+    ngdata.exit().remove();
+    publicAPI.coordsChanged();
+    console.log(model.nodeGroup.selectAll('.node'));
+  };
+}
+
+// ----------------------------------------------------------------------------
+// Object factory
+// ----------------------------------------------------------------------------
+
+const DEFAULT_VALUES = {
+  container: null,
+  color: 'inherit',
+};
+
+// ----------------------------------------------------------------------------
+
+export function extend(publicAPI, model, initialValues = {}) {
+  Object.assign(model, DEFAULT_VALUES, initialValues);
+
+  CompositeClosureHelper.destroy(publicAPI, model);
+  CompositeClosureHelper.isA(publicAPI, model, 'VizComponent');
+  CompositeClosureHelper.get(publicAPI, model, ['color', 'container']);
+
+  hyperbolicEdgeBundle(publicAPI, model);
+}
+
+// ----------------------------------------------------------------------------
+
+export const newInstance = CompositeClosureHelper.newInstance(extend);
+
+// ----------------------------------------------------------------------------
+
+export default { newInstance, extend };
