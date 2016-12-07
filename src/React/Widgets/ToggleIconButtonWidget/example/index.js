965fc4cf334c83b4780bd13bb02fca31d9baa07c
Aron Helser <aron.helser@kitware.com>
2016-12-07 10:16:53 -0500

    fix(ToggleIconButtonWidget): allow a separate disabled icon
    Enables a checkbox with an empty box icon when disabled.
    Added that to the example.

--- src/React/Widgets/ToggleIconButtonWidget/example/index.js
+++ src/React/Widgets/ToggleIconButtonWidget/example/index.js
@@ -1,6 +1,7 @@
-import ToggleIconButtonWidget   from '..';
+/* global document */
 import React                    from 'react';
 import ReactDOM                 from 'react-dom';
+import ToggleIconButtonWidget   from '..';
 
 const container = document.querySelector('.content');
 
@@ -11,16 +12,18 @@ require('font-awesome/css/font-awesome.css');
 document.body.style.padding = '10px';
 
 function onChange(value, name) {
-    console.log(name, ' => ' , value);
+  console.log(name, ' => ', value);
 }
 
+/* eslint-disable no-multi-spaces */
 ReactDOM.render(
-    <div>
-        <ToggleIconButtonWidget name='wifi'   icon='fa-wifi'      value onChange={onChange}/>
-        <ToggleIconButtonWidget name='btooth' icon='fa-bluetooth' value={false} onChange={onChange}/>
-        <ToggleIconButtonWidget name='a' icon='fa-at'  onChange={onChange}/>
-        <ToggleIconButtonWidget name='b' icon='fa-ban'    toggle onChange={onChange}/>
-        <ToggleIconButtonWidget name='c' icon='fa-bank' alwaysOn onChange={onChange}/>
-    </div>,
-    container);
+  <div>
+    <ToggleIconButtonWidget name="wifi"   icon="fa-wifi"      value onChange={onChange} />
+    <ToggleIconButtonWidget name="btooth" icon="fa-bluetooth" value={false} onChange={onChange} />
+    <ToggleIconButtonWidget name="a"      icon="fa-at"  onChange={onChange} />
+    <ToggleIconButtonWidget name="b"      icon="fa-ban"    toggle onChange={onChange} />
+    <ToggleIconButtonWidget name="c"      icon="fa-bank" alwaysOn onChange={onChange} />
+    <ToggleIconButtonWidget name="check"  icon="fa-check-square-o" iconDisabled="fa-square-o" onChange={onChange} />
+  </div>,
+  container);
 
