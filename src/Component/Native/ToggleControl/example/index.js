9b54959393b406b579f9672d9a50c39d065eca6a
Aron Helser <aron.helser@kitware.com>
2017-10-19 16:44:58 -0400

    fix(ToggleControl): shrink height, draw on top.
    Bump z-index so it draws on top of parallel coords.
    Replace set height calc with max-height and add a min-height style, so
    content resizes naturally.
    
    Tweak example so toggle panel isn't too small.

--- src/Component/Native/ToggleControl/example/index.js
+++ src/Component/Native/ToggleControl/example/index.js
@@ -1,5 +1,6 @@
 import ToggleControlComponent   from '..';
 import BGColorComponent     from '../../BackgroundColor';
+import Spacer from '../../Spacer';
 
 // Load CSS
 import 'normalize.css';
@@ -8,8 +9,9 @@ const container = document.querySelector('.content');
 container.style.height = '100vh';
 
 const green = new BGColorComponent('green');
-const red = new BGColorComponent('red');
-const toggleView = new ToggleControlComponent(green, red);
+const spacer = new Spacer('200px');
+
+const toggleView = new ToggleControlComponent(green, spacer);
 
 toggleView.setContainer(container);
 toggleView.render();
