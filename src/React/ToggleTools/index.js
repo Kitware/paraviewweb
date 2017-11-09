4fda6982ac904530ea2d9dc27a2e7802a41c0cc3
Aron Helser <aron.helser@kitware.com>
2017-11-09 13:08:19 -0500

    chore(dist): update dist

--- src/React/ToggleTools/index.js
+++ src/React/ToggleTools/index.js
@@ -1,11 +1,13 @@
 import AnnotationEditor  from './AnnotationEditor';
 import FieldSelector     from './FieldSelector';
+import ParallelCoordinates     from './ParallelCoordinates';
 import WorkbenchLayout   from './WorkbenchLayout';
 import ScatterPlotControl   from './ScatterPlotControl';
 
 export default {
   AnnotationEditor,
   FieldSelector,
+  ParallelCoordinates,
   ScatterPlotControl,
   WorkbenchLayout,
 };
