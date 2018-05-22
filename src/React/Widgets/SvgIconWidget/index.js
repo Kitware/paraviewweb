5ef17ecba4221e08cffdb4015931ad394f9e7bea
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-05-22 16:01:46 -0600

    fix(SvgIconWidget): Support old and new svg-loader

--- src/React/Widgets/SvgIconWidget/index.js
+++ src/React/Widgets/SvgIconWidget/index.js
@@ -3,13 +3,23 @@ import PropTypes from 'prop-types';
 import defaultIcon from '../../../../svg/kitware.svg';
 
 export default function SvgIconWidget(props) {
+  const { id, viewBox } = props.icon || {};
+  let xlink = id || props.icon;
+  if (xlink[0] !== '#') {
+    xlink = `#${xlink}`;
+  }
   const style = Object.assign({}, props.style, {
     width: props.width,
     height: props.height,
   });
   return (
-    <svg style={style} className={props.className} onClick={props.onClick}>
-      <use xlinkHref={props.icon} />
+    <svg
+      viewBox={viewBox || props.viewBox}
+      style={style}
+      className={props.className}
+      onClick={props.onClick}
+    >
+      <use xlinkHref={xlink} />
     </svg>
   );
 }
@@ -17,10 +27,11 @@ export default function SvgIconWidget(props) {
 SvgIconWidget.propTypes = {
   className: PropTypes.string,
   height: PropTypes.string,
-  icon: PropTypes.string,
+  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
   width: PropTypes.string,
   style: PropTypes.object,
   onClick: PropTypes.func,
+  viewBox: PropTypes.string,
 };
 
 SvgIconWidget.defaultProps = {
@@ -30,4 +41,5 @@ SvgIconWidget.defaultProps = {
   height: undefined,
   width: undefined,
   onClick: undefined,
+  viewBox: '0 0 20 20',
 };
